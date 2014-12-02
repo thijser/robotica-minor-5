3daa8c9dfb16a09831fbce92f4cad095e680bda0
Yuugoh <hugoreinbergen@hotmail.com>
2014-12-02 12:21:23 +0100

    lolololololol

--- turtleCode/build/catkin_generated/order_packages.py
+++ turtleCode/build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/thijs/codes/robotica-minor-5/turtleCode/src"
+source_root_dir = "/home/yuugo/Programming/robotica-minor-5/turtleCode/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/thijs/codes/robotica-minor-5/turtleCode/devel;/home/thijs/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/thijs/codes/robotica-minor-5/turtleCode/devel;/home/thijs/catkin_ws/devel;/opt/ros/indigo" != "" else []
+underlay_workspaces = "/home/yuugo/Programming/robotica-minor-5/turtleCode/devel;/opt/ros/indigo".split(';') if "/home/yuugo/Programming/robotica-minor-5/turtleCode/devel;/opt/ros/indigo" != "" else []
