74a465877795317afb5917cbc49eca998d4508fc
Yuugoh <hugoreinbergen@hotmail.com>
2014-12-11 14:53:55 +0100

    Updates to launch + switch communication.

--- turtleCode/src/glados/src/drivers/launchDriver.cpp
+++ turtleCode/src/glados/src/drivers/launchDriver.cpp
@@ -11,31 +11,29 @@
 using namespace std;
 
 LaunchDriver::LaunchDriver () {
-	sensor = new SwitchSensor("Rack");
 	ros::NodeHandle nh;
 	handle = nh;
 	LaunchDriver::init();
 } 
 
-LaunchDriver::LaunchDriver(SwitchSensor *s, ros::NodeHandle h){
-	sensor = s;
+LaunchDriver::LaunchDriver(ros::NodeHandle h){
 	handle = h;
 	LaunchDriver::init();
 }
 
 void LaunchDriver::init(){
-	std::fstream fs;
-	fs.open("/sys/class/gpio/export");
-	fs << 31;  							// <<< PORT
-	fs.close();
+	//std::fstream fs;
+	//fs.open("/sys/class/gpio/export");
+	//fs << 31;  							// <<< PORT
+	//fs.close();
 
-	switchSub = handle.subscribe<std_msgs::Bool>("/tawi/sensors/switch", 10, &LaunchDriver::switchCallback, this);
+	switchSub = handle.subscribe<std_msgs::Int16>("/tawi/sensors/switch", 10, &LaunchDriver::switchCallback, this);
 
 	launchSub = handle.subscribe<std_msgs::Int16>("/tawi/motors/launch", 10, &LaunchDriver::launchCallback, this);
 	ROS_INFO("launchDriver subscribers initialised");
 }
 
-void LaunchDriver::switchCallback(const std_msgs::Bool::ConstPtr &msg){
+void LaunchDriver::switchCallback(const std_msgs::Int16::ConstPtr &msg){
 	ROS_INFO("switchCallback");
 	switchReady = msg->data;
 }
@@ -53,14 +51,15 @@ bool LaunchDriver::launch(){
 	if(switchReady){
 		std::fstream fs;
 		//When specifing port you have to change the path names too!
-		
+		/*
 		fs.open("/sys/class/gpio/gpio31/direction"); //PORT 
 	   	fs << "out";
 	   	fs.close();
 	   	fs.open("/sys/class/gpio/gpio31/value"); //PORT
 	  	fs << "0"; // "1" for off
-	   	fs.close();
-	   	ROS_INFO("Sent 0 to gpio30/value");
+	   	fs.close();*/
+	   	ROS_INFO("Sent 0 to gpio31/value");
+
    	}
    	else
    		ROS_INFO("switchReady == false. Launch system still preparing");
@@ -69,9 +68,10 @@ bool LaunchDriver::launch(){
 void LaunchDriver::spin() {
 	ROS_INFO("Spinning launchDriver");
 
-	ros::Rate r(1000);
+	ros::Rate r(5);
 
 	while(ros::ok()) {
+		launch();
 
 		ros::spinOnce();
 		r.sleep();
--- turtleCode/src/glados/src/sensors/switchSensor.cpp
+++ turtleCode/src/glados/src/sensors/switchSensor.cpp
@@ -52,15 +52,15 @@ int main(int argc, char **argv) {
 	ros::NodeHandle handle;
 	ros::Publisher pub = handle.advertise<std_msgs::Int16>("/tawi/sensors/switch", 100);
 	std_msgs::Int16 msg;
-	ros::Rate loop_Rate(10);
+	ros::Rate loop_Rate(5);
 
 	while(ros::ok()){
-		int stateRead = 2; 
+		int stateRead = 1; 
 		fs.open("/sys/class/gpio/gpio60/value");   // <<< PORT
 		fs >> stateRead;
 		fs.close();
 		if(stateRead == 2)
-			ROS_INFO("stateRead still 2");
+			ROS_INFO("stateRead not set");
 		msg.data = 1-stateRead; //Inverting for BBB logic
 
 		pub.publish(msg);
