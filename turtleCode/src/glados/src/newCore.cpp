5f173fd629aff57e1e492c5ed362de1ed3584b2d
Yuugoh <hugoreinbergen@hotmail.com>
2015-01-16 14:40:35 +0100

    gaaah

--- turtleCode/src/glados/src/newCore.cpp
+++ turtleCode/src/glados/src/newCore.cpp
@@ -100,7 +100,7 @@ int stringToAscii(string s){
 
 void NewCore::serialCallback(const std_msgs::String::ConstPtr& msg){
 	ROS_INFO("msg-data is %d. Looking for %d", stringToAscii(msg->data), stringToAscii("c"));
-	if("c" == msg->data){
+	if('c' == msg->data[0]){
 		acceptBall();
 		NewCore::startConvey();
 		ask++;
