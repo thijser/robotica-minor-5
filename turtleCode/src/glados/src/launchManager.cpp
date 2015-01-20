04064395d63c6a694869e54b74ff84670efb7596
Yuugoh <hugoreinbergen@hotmail.com>
2015-01-20 10:07:49 +0100

    Gave launchManager support for ultrasonic sensors... sort of DOES NOT COMPILE OUTSIDE BBB

--- turtleCode/src/glados/src/launchManager.cpp
+++ turtleCode/src/glados/src/launchManager.cpp
@@ -26,10 +26,15 @@ void LaunchManager::coreCallback(const std_msgs::String::ConstPtr &msg){
 			conveyPub.publish(message);
 		}
 	}
-	if(!launching){
-		if("startlaunch" == msg->data){
+	if("startlaunch" == msg->data){	
+		if(ultrasoneSafe()){
 			std_msgs::Int16 message;
 			message.data = 1;
+			launchPub.publish(message);
+		}
+		else{
+			std_msgs::Int16 message;
+			message.data = 0;
 			launchPub.publish(message);
 		}
 	}
@@ -62,6 +67,52 @@ void LaunchManager::spin(){
 		ros::spinOnce();
 		rate.sleep();
 	}
+}
+
+bool LaunchManager::ultrasoneSafe(){
+	tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+	prussdrv_init();
+	/* Open PRU Interrupt */
+        if (prussdrv_open (PRU_EVTOUT_0)) {
+                // Handle failure
+                ROS_INFO(stderr, ">> PRU open failed\n");
+                return false;
+        }
+
+        /* Get the interrupt initialized */
+        prussdrv_pruintc_init(&pruss_intc_initdata);
+
+        /* Get pointers to PRU local memory */
+        void *pruDataMem;
+        prussdrv_map_prumem(PRUSS0_PRU0_DATARAM, &pruDataMem);
+        unsigned int *pruData = (unsigned int *) pruDataMem;
+
+        /* Execute code on PRU */
+        ROS_INFO(">> Executing HCSR-04 code\n");
+        prussdrv_exec_program(0, "hcsr04.bin");
+
+        // Wait for the PRU interrupt
+        prussdrv_pru_wait_event (PRU_EVTOUT_0);
+        prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+
+        // Print the distance received from the sonar
+        // At 20 degrees in dry air the speed of sound is 342.2 cm/sec
+        // so it takes 29.12 us to make 1 cm, i.e. 58.44 us for a roundtrip of 1 cm
+        ROS_INFO("Distance = %.2f cm\n", (float) pruData[0] / 58.44);
+
+        prussdrv_pru_disable(0);
+        prussdrv_exit();
+        ROS_INFO(">> PRU Disabled.\r\n");
+
+        if(( (float) pruData[0] / 58.44) < 50){
+        	return false;
+        }
+        else{
+        	return true;
+        }       
+
+        return false;
+
 }
 
 int main(int argc, char **argv){
